d800b050d5d5e3ebce8771695743c26214e4ccce
Federico Garcia <federicoignaciogarcia@gmail.com>
2022-11-10 17:59:00 -0300

    Ajuste CSS - Simulacion propiedades

--- propiedades.php
+++ propiedades.php
@@ -26,20 +26,121 @@
     <main>
         <div class="container_propiedades">
             <div class="container_categorias">
-                <ul>
-                    <label for=""></label>
-                    <li><input type="checkbox" id=""></li>
-                    <label for=""></label>
-                    <li><input type="checkbox" id=""></li>
-                    <label for=""></label>
-                    <li><input type="checkbox" id=""></li>
-                    <label for=""></label>
-                    <li><input type="checkbox" id=""></li>
-                    <label for=""></label>
-                    <li><input type="checkbox" id=""></li>
-                </ul>
+                <div class="filtros">
+                    <h2>Categorias</h2>
+                    <div class="divisor_categoria"></div>
+                    <h3>Tipo de establecimiento</h3>
+                    <ul>
+                        <li>Departamentos</li>
+                        <li>Casas</li>
+                        <li>Castillos</li>
+                    </ul>
+                    <div class="divisor_categoria"></div>
+                    <h3>Pais</h3>
+                    <ul>
+                        <li>Argentina</li>
+                        <li>España</li>
+                        <li>Alemania</li>
+                    </ul>
+                </div>
             </div>
             <div class="propiedades">
+                <h2>Departamentos</h2>
+                <div class="propiedades_seccion">
+                    <div class="propiedad">
+                        <div><img src="https://d1994bulhovht.cloudfront.net/382x200/listings/333c3895-048d-4e1e-8d1e-db7c6875d2f9/5c22bf5a-1709-46aa-8f61-961fe289a6b5.webp" alt=""></div>
+                        <div class="text">
+                            <p>43000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                        <div><img src="https://d1994bulhovht.cloudfront.net/382x200/listings/1cf7296c-7c67-4a81-bfa0-379cbe38d968/b771bb77-cf5c-4fdc-a616-77e918254841.webp" alt=""></div>
+                        <div class="text">
+                            <p>35000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                        <div><img src="https://d1994bulhovht.cloudfront.net/382x200/listings/6dce1826-a4ba-4567-ae21-93a22ed0805f/8dcc5f64-ffc4-42aa-a925-1966f97c16a8.webp" alt=""></div>
+                        <div class="text">
+                            <p>38000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                        <div><img src="https://d1994bulhovht.cloudfront.net/382x200/listings/efa21094-523b-4714-9eb8-bb05363ac62a/3967672e-03fb-4b97-95fa-94219dfbf0bf.webp" alt=""></div>
+                        <div class="text">
+                            <p>29500 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>                
+                </div>
+                <div class="divisor_categoria"></div>
+                <h2>Casas</h2>
+                <div class="propiedades_seccion">
+                    <div class="propiedad">
+                                        <div><img src="https://resizer.glanacion.com/resizer/jgCk_yI8bGEJq91pUnhtz2_c0wM=/1920x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/TIUQQVKWUNE2ZCHOHYE3P62ZKQ.jpg" alt=""></div>
+                        <div class="text">
+                            <p>830.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                                        <div><img src="https://www.bbva.com/wp-content/uploads/2021/04/casas-ecolo%CC%81gicas_apertura-hogar-sostenibilidad-certificado--1024x629.jpg" alt=""></div>
+                        <div class="text">
+                            <p>750.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                                        <div><img src="https://d1994bulhovht.cloudfront.net/856x440/listings/2decfb70-62d2-471d-a23f-0439ee58ba1f/7510bddb-507a-4db2-bf5a-d7917a10e0d5.webp" alt=""></div>
+                        <div class="text">
+                            <p>300.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                                        <div><img src="https://lavistadesaneduardo.com/wp-content/themes/san-eduardo/img/departamentos/departamentos-banner.jpg" alt=""></div>
+                        <div class="text">
+                            <p>150.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="divisor_categoria"></div>
+                <h2>Castillos</h2>
+                <div class="propiedades_seccion">
+                    <div class="propiedad">
+                        <div><img src="https://media.glamour.mx/photos/6190bef9a6e030d648105ee1/3:2/w_840,h_560,c_limit/159281.jpg" alt=""></div>
+                        <div class="text">
+                            <p>213.600.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                        <div><img src="https://www.civitatis.com/blog/wp-content/uploads/2020/12/castillo-chambord-loira.jpg" alt=""></div>
+                        <div class="text">
+                            <p>320.000.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                        <div><img src="https://imagenes.elpais.com/resizer/NtbhhXeMpzODcmHb0FL6V2C13HM=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/AROFUGK2WTTHDWYWUC2OBMQGVE.jpg" alt=""></div>
+                        <div class="text">
+                            <p>140.000.000 USD</p>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                    <div class="propiedad">
+                        <div><img src="https://www.molaunhuevo.com/wp-content/uploads/castillo-craigcrook-1-638x400.jpg" alt=""></div>
+                        <div class="text">
+                            <p>125.500.000 USD</p>
+
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvxlvItU8FUUjAmUqFg42X9wKqmwVV8i-sxAo7yIZAyg54BfwFtb4FUi5rXnIN9uHw31w&usqp=CAU" alt="">
+                        </div>
+                    </div>
+                </div>
                 <?php 
                     // $host = "localhost";
                     // $user = "root";
@@ -53,20 +154,6 @@
                     // $resultado = mysqli_query($connect,$sql);
                     // $datos= $resultado -> fetch_assoc()                    
                 ?>
-
-
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
-                <div class="propiedad"></div>
             </div>
         </div>
     </main>
